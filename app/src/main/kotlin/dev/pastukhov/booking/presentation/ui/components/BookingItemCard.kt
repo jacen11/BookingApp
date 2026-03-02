46eca02ab43d737d251d0c1c4f16630be5652def
Пастухов Дмитрий Сергеевич <dspastukhov@alfabank.ru>
2026-03-02 19:57:13 +0300

    change package java to kotlin

--- app/src/main/kotlin/dev/pastukhov/booking/presentation/ui/components/BookingItemCard.kt
+++ app/src/main/kotlin/dev/pastukhov/booking/presentation/ui/components/BookingItemCard.kt
@@ -15,9 +15,7 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.Chat
-import androidx.compose.material.icons.filled.CalendarMonth
 import androidx.compose.material.icons.filled.Call
-import androidx.compose.material.icons.filled.Chat
 import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
